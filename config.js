dde90c1d2e4ffc1d876d0de12467937c05431283
ibrahim9311 <146417581+ibrahim9311@users.noreply.github.com>
2025-10-07 22:14:59 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+export const SUPABASE_URL = 'https://uulzslgyqygvtpfgeecr.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1bHpzbGd5cXlndnRwZmdlZWNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NzM4ODEsImV4cCI6MjA3NTM0OTg4MX0.VLJ_R6aKKz39AvZphb0JGVspUpw--l2JNzrk5yuNwkQ';
